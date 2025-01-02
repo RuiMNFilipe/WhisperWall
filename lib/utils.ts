8371a770d48137864367c8a428d2def393386796
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-02 15:53:31 +0000

    Add setLocaleDateAndTime function

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,3 +10,11 @@ export function trimContentSize(content: string, maxChars: number) {
     ? `${content.slice(0, maxChars)}...`
     : content;
 }
+
+export const setLocaleDateAndTime = (date: Date) => {
+  const fullDate = date.toLocaleDateString("pt-PT");
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+
+  return `${fullDate} ${hours}:${minutes}`;
+};
