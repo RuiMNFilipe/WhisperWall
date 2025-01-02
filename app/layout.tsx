90f4956cd548d3cc1e3bd16e23dbbca27e05d04a
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-02 22:06:28 +0000

    Set toast notifications position to bottom-right

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <ToastContainer position="top-center" />
+        <ToastContainer position="bottom-right" />
       </body>
     </html>
   );
