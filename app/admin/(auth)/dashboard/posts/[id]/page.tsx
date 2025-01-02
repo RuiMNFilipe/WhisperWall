e97838cbdc299be6c2eb8414cd24786cdb6af3e5
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-02 13:42:52 +0000

    Add toast message on error

--- app/admin/(auth)/dashboard/posts/[id]/page.tsx
+++ app/admin/(auth)/dashboard/posts/[id]/page.tsx
@@ -2,20 +2,30 @@
 
 import { getPost } from "@/actions/get-post";
 import ReplyForm from "@/components/ReplyForm";
+import { Post } from "@prisma/client";
+import { toast } from "react-toastify";
 
 async function PostDetailPage({ params }: { params: { id: string } }) {
   const { id } = await params;
-  const post = await getPost(Number(id));
-  if (!post) return <div>Post not found.</div>;
+  const { success, data, message } = await getPost(Number(id));
+
+  if (!success || !data) {
+    toast.error(message);
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        ´{message}
+      </div>
+    );
+  }
 
   return (
     <section className="flex flex-col h-screen w-screen items-center justify-center">
       <h1>Post Details</h1>
       <div className="flex flex-col w-[70%] mx-auto border border-slate-400 rounded-xl bg-slate-600 gap-10 p-10">
         <div className="w-full text-center text-white">
-          <p>{post.content}</p>
+          <p>{(data as Post).content}</p>
         </div>
-        <ReplyForm post={post} />
+        <ReplyForm post={data as Post} />
       </div>
     </section>
   );
