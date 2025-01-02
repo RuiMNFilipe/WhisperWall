c30dd86255f944a23b7b49a8ebbed7c845486d79
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-02 21:08:45 +0000

    Add utility function to convert milliseconds to day, hour, minute and second

--- lib/utils.ts
+++ lib/utils.ts
@@ -20,3 +20,12 @@ export const setLocaleDateAndTime = (date: Date) => {
 
   return `${fullDate} ${hours}:${minutes}`;
 };
+
+export const convertMilliseconds = (ms: number) => {
+  const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((ms % (1000 * 60)) / 1000);
+
+  return { days, hours, minutes, seconds };
+};
