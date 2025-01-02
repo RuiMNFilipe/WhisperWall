0e8fe062341b0d403f6e890042957510b5154b63
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-02 22:03:48 +0000

    Add dynamic cols size to grid

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,31 @@ import { getAnsweredPostsAction } from "@/actions/get-answered-posts";
 import PostForm from "@/components/PostForm";
 import PostsList from "@/components/PostsList";
 import { Post } from "@prisma/client";
-import { toast } from "react-toastify";
 
 export default async function Home() {
   const result = await getAnsweredPostsAction();
 
-  if (!result.success) toast.error(result.message);
+  const getNumberOfColsClass = (length: number) => {
+    switch (length) {
+      case 1:
+        return "grid-cols-1";
+      case 2:
+        return "grid-cols-2";
+      case 3:
+        return "grid-cols-3";
+      default:
+        return "grid-cols-4";
+    }
+  };
+
+  const numberOfColsClass = getNumberOfColsClass(
+    (result.data as Post[]).length
+  );
 
   return (
     <main className="bg-slate-400 h-screen py-8">
       <PostForm />
-      <div className="grid grid-cols-4 gap-y-10">
+      <div className={`grid ${numberOfColsClass} gap-y-10`}>
         <PostsList
           answeredPosts={result.success ? (result.data as Post[]) : []}
         />
