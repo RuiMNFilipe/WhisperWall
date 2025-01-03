fe27a3c94fd01c5f8ec817190ca8f8ba55a5136c
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-03 17:14:32 +0000

    Removed Role fetching logic

--- components/ModDashboardHeader.tsx
+++ components/ModDashboardHeader.tsx
@@ -3,44 +3,17 @@
 import Link from "next/link";
 import Image from "next/image";
 import { FaSignOutAlt } from "react-icons/fa";
-import { logoutModAction } from "@/actions/logout-mod";
-import { redirect } from "next/navigation";
 import { useFormStatus } from "react-dom";
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
-import { getRoleAction } from "@/actions/get-role";
 import { Role } from "@prisma/client";
+import { usePathname } from "next/navigation";
 
-function ModDashboardHeader() {
+interface ModDashboardHeaderProps {
+  userRole: Role | null;
+  onLogout: () => void;
+}
+function ModDashboardHeader({ userRole, onLogout }: ModDashboardHeaderProps) {
   const { pending } = useFormStatus();
-  const [userRole, setUserRole] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchUserRole = async () => {
-      const result = await getRoleAction();
-
-      if (result.success && result.role) {
-        setUserRole(result.role);
-      } else {
-        console.error(result.message);
-        toast.error(result.message);
-      }
-    };
-
-    fetchUserRole();
-  }, [userRole]);
-
-  const handleLogout = async () => {
-    const result = await logoutModAction();
-
-    if (result.success && result.redirectTo) {
-      toast.success("Sessão terminada com sucesso!");
-      redirect(result.redirectTo);
-    } else {
-      toast.error(result.message);
-      redirect(result.redirectTo!);
-    }
-  };
+  const path = usePathname();
 
   return (
     <header className="flex justify-between items-center px-10">
@@ -52,13 +25,17 @@ function ModDashboardHeader() {
           height={50}
         />
       </Link>
-      {userRole === Role.ADMIN && (
-        <Link href={"panel"}>Painel Administração</Link>
+      {path === "/admin" ? null : (
+        <>
+          {userRole === Role.ADMIN && (
+            <Link href={"panel"}>Painel Administração</Link>
+          )}
+          <Link href={"/admin/dashboard"}>Dashboard</Link>
+          <button onClick={onLogout} disabled={pending}>
+            <FaSignOutAlt />
+          </button>
+        </>
       )}
-      <Link href={"/admin/dashboard"}>Dashboard</Link>
-      <button onClick={handleLogout} disabled={pending}>
-        <FaSignOutAlt />
-      </button>
     </header>
   );
 }
