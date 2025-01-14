8cf675451169aad5fa1d4b600837bf8a6c65be1f
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-14 13:06:55 +0400

    Create updateUserRoleAction

---
+++ actions/update-user-role.ts
@@ -0,0 +1,52 @@
+"user server";
+
+import { ServerActionFeedback } from "@/types";
+import { Role } from "@prisma/client";
+import { splitSessionAndRole } from "./utils";
+import { prisma } from "@/db/db";
+
+export const updateUserRoleAction = async (
+  id: number,
+  newRole: Role
+): Promise<ServerActionFeedback> => {
+  try {
+    const [sessionToken, role] = await splitSessionAndRole("sessionToken");
+
+    if (!sessionToken || !role || role !== Role.ADMIN) {
+      return {
+        success: false,
+        message: "Apenas administradores podem executar esta ação.",
+      };
+    }
+
+    const user = await prisma.moderator.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      return {
+        success: false,
+        message: "Utilizador não encontrado.",
+      };
+    }
+
+    await prisma.moderator.update({
+      where: { id },
+      data: {
+        role: newRole,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Utilizador atualizado com sucesso.",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message:
+        "Ocorreu um erro ao atualizar o utilizador. Por favor, tente novamente.",
+    };
+  }
+};
