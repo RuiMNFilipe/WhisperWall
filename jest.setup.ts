cf42759743713cb3f2699cde777a585da83562e3
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-21 15:38:34 +0000

    Add react-portal mock to jest setup file

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+
+jest.mock("@radix-ui/react-portal", () => {
+  return {
+    __esModule: true,
+    Portal: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
