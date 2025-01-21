92e57af4e718559c7bb199963687b804c25626e6
RuiMNFilipe <netofilipe_6@hotmail.com>
2025-01-21 14:09:35 +0000

    Add role to root div to be able to test

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,10 @@ interface CardProps {
 
 const Card = ({ content, answer }: CardProps) => {
   return (
-    <div className="justify-self-center flex flex-col items-center w-72 bg-[#ebd741] p-5 gap-y-10 rounded-lg shadow-md">
+    <div
+      role="card"
+      className="justify-self-center flex flex-col items-center w-72 bg-[#ebd741] p-5 gap-y-10 rounded-lg shadow-md"
+    >
       <div>
         <h2>{content}</h2>
       </div>
